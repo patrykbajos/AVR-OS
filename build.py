8b358fa8b2e7e24c48a5722bb3089ecbbab0fed1
patryk_bajos <patryk.bajos@gmail.com>
2018-08-28 23:08:23 +0200

    Build system

--- build.py
+++ build.py
@@ -1,10 +1,5 @@
 from pyavrutils import AvrGcc
-
-def get_module_conf(module):
-    return json.read(open('{}/module.json'.format(module)))
-
-def get_build_conf():
-    return json.read(open('build.json'))
+from build_utils import *
 
 cc = AvrGcc()
 cc.optimize_for_size()
--- build_utils.py
+++ build_utils.py
@@ -1,7 +1,7 @@
 import json
 
 def get_module_conf(module):
-    return json.read(open('{}/module.json'.format(module)))
+    return json.load(open('{}/module.json'.format(module)))
 
 def get_build_conf():
-    return json.read(open('build.json'))
+    return json.load(open('build.json'))
